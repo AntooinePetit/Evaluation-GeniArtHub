f80b24fd86a4f5ba8c88d3887b1fe10f5fcf3cd6
Antoine Petit <antooine.petit@gmail.com>
2025-06-04 16:36:39 +0200

    feat(front): display cart items dynamically
    - Created cart.js to fetch and display cart items.
    - Implemented dynamic rendering of cart products.
    - Removed hardcoded cart items from cart.html.
    - Added error handling for empty cart scenarios.
    - Uses data from a local API for product details.

---
+++ front/js/cart.js
@@ -0,0 +1,63 @@
+// Récupération des données globales de tous les produits
+async function getDatas() {
+  try {
+    const req = await fetch("http://localhost:3000/api/products/");
+    return await req.json();
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+// Affichage des données du produit
+async function populateDatas() {
+  try {
+    const datas = await getDatas();
+    if (panier.length === 0) {
+      document.querySelector("#panier > div").insertAdjacentHTML(
+        "afterbegin",
+        `
+      <article>
+         <p>Votre panier est vide. Veuillez ajouter au moins un article à votre panier</p>
+      </article>`
+      );
+      console.log('panier vide')
+      return;
+    }
+    panier.forEach((produit) => {
+      const { idCart, id, taille, quantite } = produit;
+      const produitRecherche = datas.find((prod) => prod._id === id);
+      const { titre, image, declinaisons } = produitRecherche;
+      const tailleChoisie = declinaisons[taille]
+      const { prix } = tailleChoisie;
+      const template = `<article>
+            <img src="${image}" alt="${titre}">
+            <h2>${titre}</h2>
+            <p>${taille}</p>
+            <p>${prix}€</p>
+            <div class="input-container">
+               <label for="quantity-${idCart}">Quantité : </label>
+               <input type="number" name="quantity-${idCart}" id="quantity-${idCart}"  placeholder="${quantite}" value="${quantite}" minlength="1">
+            </div>
+            <button id="delete-bird">Supprimer</button>
+          </article>`;
+      document.querySelector("#panier > div").insertAdjacentHTML('afterbegin', template)
+      console.log(produitRecherche, tailleChoisie);
+    });
+  } catch (e) {
+    console.error(e)
+    document.querySelector("#panier > div").insertAdjacentHTML(
+      "afterbegin",
+      `
+      <article>
+         <p>Votre panier est vide. Veuillez ajouter au moins un article à votre panier</p>
+      </article>`
+    );
+  }
+}
+
+// Récupération du panier dans le local storage s'il existe, sinon création d'un panier vide
+const panier = localStorage.getItem("panier")
+  ? JSON.parse(localStorage.getItem("panier"))
+  : [];
+
+populateDatas();
