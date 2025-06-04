eeb82c8d8a027734fa60e6edd008c23df4307294
Antoine Petit <antooine.petit@gmail.com>
2025-06-04 10:01:52 +0200

    fix: handle API errors and display message
    - Improved error handling in getDatas
    - Added error display in populateDatas
    - Display error message if API request fails
    - Added CSS styling for error message
    - Used try...catch block for better error handling

--- front/js/index.js
+++ front/js/index.js
@@ -1,22 +1,31 @@
 async function getDatas() {
-  const req = await fetch("http://localhost:3000/api/products/");
-  const res = await req.json();
-  return res;
+  try {
+    const req = await fetch("http://localhost:3000/api/products/");
+    const res = await req.json();
+    return res;
+  } catch (e) {
+    console.error(e);
+  }
 }
 
 async function populateDatas() {
   const datas = await getDatas();
   const sectionProducts = document.querySelector(".products");
-  datas.forEach((data) => {
-    const { _id, shorttitle, titre, image } = data;
-    const template = `
-            <article>
-                <img src="${image}" alt="${titre}">
-                <a href="product.html?id=${_id}">Buy ${shorttitle} </a>
-            </article>
-    `;
-    sectionProducts.insertAdjacentHTML('beforeend', template)
-  });
+  try {
+    datas.forEach((data) => {
+      const { _id, shorttitle, titre, image } = data;
+      const template = `
+              <article>
+                  <img src="${image}" alt="${titre}">
+                  <a href="product.html?id=${_id}">Buy ${shorttitle} </a>
+              </article>
+      `;
+      sectionProducts.insertAdjacentHTML("beforeend", template);
+    });
+  } catch (e) {
+    console.error(e)
+    sectionProducts.innerHTML = '<p class="error">Erreur ! Aucun article trouvé !</p>'
+  }
 }
 
 populateDatas();
